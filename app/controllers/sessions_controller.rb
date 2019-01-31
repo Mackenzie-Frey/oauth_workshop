bc5507c480e61527feb6b8a124b397b98d7a95cc
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-31 11:11:48 -0700

    Implements a migration for a User and begins finding or creating by the user id found in the parsed JSON

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,30 @@
 class SessionsController < ApplicationController
   def create
-    binding.pry
-  end
+    client_id     = 'a310b87f10ce6f33876f'
+    client_secret = '64ff40de541bf6050436e489c8387bde3e784b9b'
+    code          = params[:code]
+    response      = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
+
+    pairs = response.body.split("&")
+      response_hash = {}
+      pairs.each do |pair|
+        key, value = pair.split("=")
+        response_hash[key] = value
+      end
+
+      token = response_hash["access_token"]
+
+      oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+
+      auth = JSON.parse(oauth_response.body)
+
+      user          = User.find_or_create_by(uid:auth["id"])
+      user.username = auth["login"]
+      user.uid      = auth["id"]
+      user.token    = token
+      user.save
+
+      binding.pry
+
+    end
 end
